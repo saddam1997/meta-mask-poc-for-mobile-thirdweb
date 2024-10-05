63684224c4a56fe7316c889663614de2316efefe
saddam1997 <saddam.husen1997@gmail.com>
2024-10-05 17:34:21 +0530

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,65 @@
 "use client";
+import { createThirdwebClient, getContract, prepareContractCall, toWei } from "thirdweb";
+import { ConnectButton, useActiveAccount, useWalletBalance, useSendTransaction } from "thirdweb/react";
+import { bscTestnet } from "thirdweb/chains";
+import { createWallet } from "thirdweb/wallets";
 
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
-import { client } from "./client";
+const client = createThirdwebClient({
+  clientId: "acbda4ca6f743f0357967452ead6731c",
+});
+
+const contract = getContract({
+  address: "0xd8cBA69b4c7E56Af7F024fE5e59E9619Dd251B28",
+  chain: bscTestnet,
+  client,
+});
+
+const wallets = [
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow"),
+  createWallet("io.rabby"),
+  createWallet("io.zerion.wallet"),
+  createWallet("walletConnect"),
+];
 
 export default function Home() {
+  const { mutate: sendTransaction } = useSendTransaction();
+  const account = useActiveAccount();
+  const { data: balance, isLoading } = useWalletBalance({
+    client,
+    chain: bscTestnet,
+    address: account?.address,
+  });
+
+  const { mutate: sendTx, data: transactionResult } = useSendTransaction();
+
+  const onClick = () => {
+    const transaction = prepareContractCall({
+      contract,
+      method: "function transfer(address to, uint256 value)",
+      params: ["0x4081444763A8d6E2f1476C4357b6E46292Fa5feC", toWei("100")],
+    })
+
+    sendTx(transaction);
+  };
+
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        {/* <Header /> */}
+    <div>
 
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
-
-          
-        </div>
-
-        {/* <ThirdwebResources /> */}
-      </div>
-    </main>
-  );
-}
-
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+        connectButton={{ label: "Connect Your Wallet" }}
+        connectModal={{ size: "compact" }}
       />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
+      <p>Wallet address: {account?.address}</p>
+      <p>
+        Wallet balance: {balance?.displayValue} {balance?.symbol}
       </p>
-    </header>
-  );
-}
 
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
+      <button onClick={onClick}>Transfer Tokens</button>
     </div>
-  );
-}
 
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
   );
 }
